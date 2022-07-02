1f450a6eb65eb37f99a14b032228b9b732a311d2
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-07-02 12:27:08 +0200

    feat: implement Supabase in project

--- src/supabase/init.js
+++ src/supabase/init.js
@@ -1,1 +1,8 @@
+import { createClient } from "@supabase/supabase-js"
 
+const supabaseUrl = import.meta.env.VUE_APP_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VUE_APP_SUPABASE_ANON_KEY
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
+export default supabase
