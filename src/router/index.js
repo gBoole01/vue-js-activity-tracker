f04d18b835fcc8c242abbe8fb424c6f2d9fff240
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-07-04 17:38:47 +0200

    feat: add Page Titles and Route Guards

--- src/router/index.js
+++ src/router/index.js
@@ -1,36 +1,56 @@
 import { createRouter, createWebHistory } from "vue-router";
+import supabase from '../supabase/init'
 import Home from "../views/Home.vue";
 import Login from "../views/Login.vue"
 import Register from "../views/Register.vue"
 import Create from "../views/Create.vue"
 import ViewWorkout from "../views/ViewWorkout.vue"
 
-
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
+    meta: {
+      title: "Home",
+      auth: false,
+    },
   },
   {
     path: "/login",
     name: "Login",
     component: Login,
+    meta: {
+      title: "Login",
+      auth: false,
+    },
   },
   {
     path: "/register",
     name: "Register",
     component: Register,
+    meta: {
+      title: "Register",
+      auth: false,
+    },
   },
   {
     path: "/create",
     name: "Create",
     component: Create,
+    meta: {
+      title: "Create Workout",
+      auth: true,
+    },
   },
   {
     path: "/view-workout/:workoutId",
     name: "View-Workout",
     component: ViewWorkout,
+    meta: {
+      title: "View Workout",
+      auth: false,
+    },
   },
 ];
 
@@ -40,7 +60,24 @@ const router = createRouter({
 });
 
 // Change document titles
+router.beforeEach((to, _from, next) => {
+  document.title = `${to.meta.title} | Active Tracker`
+  next()
+})
 
 // Route guard for auth routes
+router.beforeEach((to, _from, next) => {
+  const user = supabase.auth.user()
+
+  if (to.matched.some((response) => response.meta.auth)) {
+    if (!user) {
+      next({ name: 'Login' })
+      return
+    }
+    next()
+    return
+  }
+  next()
+})
 
 export default router;
